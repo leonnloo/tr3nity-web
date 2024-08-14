3c9fc8658a89046280bc6b782c506dd7c1672fc3
leonnloo <looyang01@gmail.com>
2024-08-14 14:58:07 +0800

    Added page margin

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,17 @@ import ProposalCard from "@/components/proposal-card";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ProposalCard />
-      <ProposalCard showDetails={true} />
+    <main className="min-h-screen">
+      <div className="bg-black w-full h-64 mb-5">
+        <div className="text-white text-4xl flex-center h-full">69</div>
+      </div>
+      <div className="pm ">
+        <div className="h-32 bg-blue-200 flex-center">
+          Use pm in tailwind for page x margin
+        </div>
+        <ProposalCard />
+        <ProposalCard showDetails={true} />
+      </div>
     </main>
   );
 }
